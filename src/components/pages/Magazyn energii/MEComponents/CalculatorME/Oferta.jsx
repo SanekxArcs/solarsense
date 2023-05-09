4ba8b654a8bd3dbb73249172ea43b99607bc5f9c
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-05-09 07:07:08 +0200

    Create Oferta.jsx

---
+++ src/components/pages/Magazyn energii/MEComponents/CalculatorME/Oferta.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Oferta = () => {
+  return (
+    <div>
+        <div>
+            <h5>
+            Bezpłatna oferta już do 2 dni
+            </h5>
+            <p>Wyślij formularz, a my prześlemy Ci w wiadomości mailowej szczegóły kalkulacji i do 48 godzin skontaktujemy się z Tobą telefonicznie w celu przygotowania specjalnej oferty dopasowanej do Twoich potrzeb.</p>
+            <div>
+                <h6>
+                Podsumowanie kalkulacji
+                </h6>
+                <ul>
+                    <li>
+                    Oszczędności w ciągu roku: 498 zł
+                    </li>
+                    <li>
+                    Dotacja Mój Prąd 5.0: 26 000 zł 
+                    </li>
+                </ul>
+                <div>
+                    <p>
+                    Szczegóły kalkulacji
+                    </p>
+<button>
+                    Rozwin | Zwin
+                </button>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                        Twoje zapotrzebowanie energetyczne:
+                        </li>
+                        <li>Opłaty w 2023 r. bez fotowoltaiki:</li>
+                        <li>Sugerowana moc instalacji fotowoltaicznej:</li>
+                        <li>Założona pojemność magazynu energii:</li>
+                        <li>Spodziewana autokonsumpcja:</li>
+                        <li>Energia sprzedana:</li>
+                        <li>Energia kupiona:</li>
+                    </ul>
+                </div>
+                <p>Więcej informacji na temat tej kalkulacji otrzymasz w wiadomości mailowej.</p>
+            </div>
+        </div>
+        <div>Kontakt</div>
+    </div>
+  )
+}
+
+export default Oferta
