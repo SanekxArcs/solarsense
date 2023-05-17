9ece8a7f5c96244b0c48e5e74cdca1a1472feed0
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-05-17 13:46:58 +0200

    add build

--- dist/assets/TopImg-8ba38287.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-4bbd65bf.js";const t="/assets/wave-haikei-831bb38d.svg",e=()=>s("div",{className:" relative",children:s("img",{className:"absolute top-0 left-0 right-0 w-full -z-40",src:t,alt:""})});export{e as T};
