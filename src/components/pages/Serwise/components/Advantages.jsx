f71b22d8840051722638bead0f88fae9f2fa9bd3
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-06-02 17:54:25 +0200

    Update Advantages.jsx

--- src/components/pages/Serwise/components/Advantages.jsx
+++ src/components/pages/Serwise/components/Advantages.jsx
@@ -8,12 +8,12 @@ const Advantages = () => {
         <section className="my-32 text-port-gore-900">
           <div className="block bg-white rounded-sm shadow-lg">
             <div className="flex flex-wrap items-center">
-              <div className="hidden w-full grow-0 shrink-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
+              <div className="hidden w-full lg:flex lg:w-6/12 xl:w-4/12">
                 <img
                   loading="lazy"
                   src={photoSerwis}
-                  alt="Trendy Pants and Shoes"
-                  className="object-cover w-full rounded-t-sm lg:rounded-tr-none lg:rounded-bl-sm"
+                  alt="solar panel work man"
+                  className="object-cover w-full h-full rounded-t-sm lg:rounded-tr-none lg:rounded-bl-sm"
                 />
               </div>
               <div className="w-full grow-0 shrink-0 basis-auto lg:w-6/12 xl:w-8/12">
@@ -128,56 +128,12 @@ const Advantages = () => {
                       </p>
                     </div>
                   </div>
-                  <a
-                    href="#contact"
-                    className="inline-block py-2 text-sm font-bold leading-snug uppercase transition-all duration-150 ease-in-out border rounded-sm shadow-md text-ocean-green-50 border-ocean-green-700 hover:shadow-md hover:shadow-ocean-green-100 bg-ocean-green-500 px-7 hover:bg-ocean-green-700 focus:bg-ocean-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-ocean-green-800 active:shadow-lg"
-                    data-mdb-ripple="true"
-                    data-mdb-ripple-color="light"
-                  >
-                    Kontakt
-                  </a>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <section className="mb-0 text-center text-gray-800 md:mb-32">
-          <div className="flex justify-center">
-            <div className="max-w-[1000px]">
-              <h3 className="mb-10 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
-                CENNIK
-              </h3>
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h4 className="pb-5 text-xl font-semibold">PRZEGLĄD INSTALACJI</h4>
-            <div className="flex justify-between px-4 py-3 font-medium rounded-sm shadow bg-ocean-green-300">
-              <p>Moc zestawu</p>
-              <p>Cena brutto</p>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-white rounded-sm shadow">
-              <p>Do 10 kW</p>
-              <p>442 zł</p>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-white rounded-sm shadow">
-              <p>10 - 20 kW</p>
-              <p>664 zł</p>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-white rounded-sm shadow">
-              <p>20 - 30 kW</p>
-              <p>775 zł</p>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-white rounded-sm shadow">
-              <p>30 - 40 kW</p>
-              <p>948 zł</p>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-white rounded-sm shadow">
-              <p>40 - 50 kW</p>
-              <p>1 168 zł</p>
-            </div>
-          </div>
 
-        </section>
       </div>
     </>
   );
