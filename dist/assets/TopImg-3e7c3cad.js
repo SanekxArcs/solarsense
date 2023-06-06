9ff8860760b3f4ceaf4b688826201d677e0afcd8
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-06-07 00:21:08 +0200

    add build for netlify

--- dist/assets/TopImg-3e7c3cad.js
+++
@@ -1,1 +0,0 @@
-import{j as t,t as s}from"./index-ba16c7d7.js";const e=()=>t("div",{className:" relative",children:t("img",{className:"absolute top-0 left-0 right-0 w-full -z-50",src:s,alt:""})});export{e as T};
