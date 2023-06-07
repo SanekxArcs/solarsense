2567ff8aaf925b8c6585aee7af8ff2cd4e073fc3
SanekxArcs <56607007+SanekxArcs@users.noreply.github.com>
2023-06-07 11:52:30 +0200

    change price

--- dist/assets/TopImg-3aefc0c4.js
+++
@@ -1,1 +0,0 @@
-import{j as t,t as s}from"./index-a04f426a.js";const e=()=>t("div",{className:"relative ",children:t("img",{className:"absolute top-0 left-0 right-0 w-full -z-50",src:s,alt:""})});export{e as T};
---
+++ dist/assets/bialas-6de57813.js
@@ -0,0 +1,1 @@
+const a="/assets/bialas-99013acf.webp";export{a as b};
